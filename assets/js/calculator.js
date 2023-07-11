4a885a60801a16b4d1aac94419ae584560879857
Arseniy Olevskiy <77734966+arolus@users.noreply.github.com>
2023-07-11 09:02:21 +0500

    regular commit

---
+++ assets/js/calculator.js
@@ -0,0 +1,52 @@
+function calculate(){
+  const toHex = dec => math.hex(dec).replace('0x','').toUpperCase();
+  const inputHex = (elementId, optional) => {
+    let value = input.get(elementId).raw();
+    if(optional && !value) return '';
+    if(!/^[0-9a-fA-F]+$/.test(value)) {
+      input.error(elementId, `The "${elementId}" must be a natural hex number.`);
+    }
+    return value;
+  };
+  switch($('[data-tab].tab--active').dataset.tab){
+    case '0': 
+      // 1. init & validate
+      const hex = inputHex('hex', true).toUpperCase();
+      const dec = input.get('dec').optional().natural().raw();
+      input.silent = false;
+      if(!hex && !dec){
+        input.error('hex');
+        input.error('dec');
+      }
+      if(!input.valid()) return;
+
+      // 2. calculate
+      const hexToDec = hex ? `${hex} = ${math.number('0x'+hex)}` : '';
+      const decToHex = dec ? `${dec} = ${toHex(dec)}` : '';
+      
+      // 3. output
+      _('hex_to_dec').innerHTML = hexToDec; 
+      _('dec_to_hex').innerHTML = decToHex; 
+    break;
+    case '1': 
+      // 1. init & validate
+      const hexA = inputHex('hex_a').toUpperCase(), decA = math.number('0x'+hexA);
+      const hexB = inputHex('hex_b').toUpperCase(), decB = math.number('0x'+hexB);
+      const operation = input.get('math_operation').raw();
+      if(hexB == 0 && operation == 'divide') input.error('hex_b', 'Division by zero in hex_b');
+      if(!input.valid()) return;
+      const operationsMap = {'add':'+','subtract':'−','multiply':'×','divide':'÷'};
+
+      // 2. calculate
+      let hexResult = `${hexA} ${operationsMap[operation]} ${hexB} = ${toHex(math[operation](decA,decB))}`;
+      let decResult = `${decA} ${operationsMap[operation]} ${decB} = ${math[operation](decA,decB)}`;
+      if(operation == 'divide' && math.mod(decA,decB) != 0) {
+        hexResult+= ` or ${toHex(math.divide(decA-math.mod(decA,decB),decB))} remainder ${toHex(math.mod(decA,decB))}`;
+        decResult+= ` or ${math.divide(decA-math.mod(decA,decB),decB)} remainder ${math.mod(decA,decB)}`;
+      }
+      // 3. output
+      _('hex_result').innerHTML = hexResult;
+      _('dec_result').innerHTML = decResult;
+    break;
+  }
+}
---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== Hex Calculator by Calculator.iO ===
+Contributors: calculatorio
+Tags: 
+Requires at least: 5.0
+Tested up to: 6.2.2
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
+Online hex calculator performs hex math operations and conversions. Hex addition, hex subtraction, hex multiplication, hex division became fast & easy
+
+== Description ==
+
+Online hex calculator performs hex math operations and conversions. Hex addition, hex subtraction, hex multiplication, hex division became fast & easy
+
+== Installation ==
+
+1. Upload the /ci_hex_calculator/ folder to the /wp-content/plugins/ directory.
+2. Activate the [Hex Calculator](https://www.calculator.io/hex-calculator/ "Hex Calculator Homepage") plugin through the "Plugins" menu in WordPress.
+3. Configure your settings via Appearance > Widgets > Hex Calculator.
+4. And you're good to go!
+
+== Screenshots ==
+1. The Hex Calculator Input Form.
+2. The Hex Calculator Calculation Results.
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release
