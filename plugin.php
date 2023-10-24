785f2371a1fb6a521b2df6ecffdca92722958da9
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:09:09 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_hex_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Hex Calculator by Calculator.iO";
 
 function display_ci_hex_calculator(){
